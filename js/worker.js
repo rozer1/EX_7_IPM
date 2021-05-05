a9f298229d9b9a8a29d39555f0a5f694f97919b7
rozer1 <239577@edu.p.lodz.pl>
2021-05-05 10:13:22 +0200

    Worker fix

--- js/worker.js
+++ js/worker.js
@@ -1,3 +1,23 @@
 onmessage = function (e) {
-    console.log(JSON.parse(e.data));
+  var contact = e.data;
+  console.log("in: ", contact);
+
+  for(var key in cuontact){
+    contact[key] = reverseText(contacy[key]);
+  }
+
+  var output = '';
+  console.log("out: ", output);
 };
+
+function reverseText(text){
+  for (var i = 0, i < text.length; i++) {
+    var character = text[i];
+    if (character == character.toLowerCase()) { // The character is lowercase
+      output += character.toUpperCase();
+    } else { // The character is uppercase
+      output += character.toLowerCase();
+    }
+  }
+  return output
+}
